91d8f464a5c96aa3cc9593d3ff88662e9e3c6104
xiaoming <1658471647@qq.com>
2021-07-24 09:13:19 +0800

    fix:autoComplete组件处理复杂数据类型和自定义下拉选项

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -1,22 +1,48 @@
 import React from 'react'
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import AutoComplete from './autoComplete'
+import AutoComplete, { DataSourceType } from './autoComplete'
+
+interface LakerPlayerProps {
+    value: string;
+    number: number;
+}
 
 const SimpleComplete = () => {
+    //简单数据结构
     let lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins', 'james', 'AD',
         'green', 'howard', 'kuzma', 'McGee', 'rando'
     ]
+    //复杂的数据结构
+    const lakersWithNumber = [
+        { value: 'bradley', number: 11 },
+        { value: 'pope', number: 1 },
+        { value: 'caruso', number: 4 },
+        { value: 'cook', number: 2 },
+        { value: 'cousins', number: 15 },
+        { value: 'james', number: 23 },
+        { value: 'AD', number: 3 },
+        { value: 'green', number: 14 },
+        { value: 'howard', number: 39 },
+        { value: 'kuzma', number: 0 }
+    ]
     const handleFetchValue = (query: string) => {
-        return lakers.filter((name: string) => name.includes(query))
+        return lakers.filter((name: string) => name.includes(query)).map(name => ({ value: name }))
     }
-    const renderOptions = (item: string) => {
-        return <h3>name:{item}</h3>
+    const handleFetchObjectValue = (query: string) => {
+        return lakersWithNumber.filter((player) => player.value.includes(query))
+    }
+    const renderOptions = (item: DataSourceType<LakerPlayerProps>) => {
+        return <>
+            <h3>name:{item.value}</h3>
+            <p>number:{item.number}</p>
+        </>
     }
     return <AutoComplete
         fetchSuggest={handleFetchValue}
         onSelect={action('selected')}
-        renderOptions={renderOptions}
+        // fetchSuggest={handleFetchObjectValue}
+        // renderOptions={renderOptions}
     />
 }
 
--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,21 +1,27 @@
 import React, { useState, FC, ChangeEvent, ReactElement } from 'react'
 import { Input, InputProps } from "../Input/input"
 
+interface DataSourceObj {
+    value: string;
+}
+
+export type DataSourceType<T = {}> = T & DataSourceObj
+
 // 1.定义autoComplete组件参数的类型
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-    fetchSuggest: (str: string) => string[];
-    onSelect?: (item: string) => void;
-    renderOptions?: (item: string) => ReactElement;
+    fetchSuggest: (str: string) => DataSourceType[];
+    onSelect?: (item: DataSourceType) => void;
+    renderOptions?: (item: DataSourceType) => ReactElement;
 }
 
 //2.定义autoComplete组件
-const AutoComplete: FC<AutoCompleteProps> = (props) => {
+export const AutoComplete: FC<AutoCompleteProps> = (props) => {
     const { fetchSuggest, onSelect, value, renderOptions, ...restProps } = props;
 
     // 2.1定义input输入框中的值
     const [inputValue, setInputValue] = useState(value)
     // 2.2定义搜索下拉框中的值
-    const [suggestions, setSuggestions] = useState<string[]>([])
+    const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
 
     // 3.保存用户输入的值
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -42,15 +48,15 @@ const AutoComplete: FC<AutoCompleteProps> = (props) => {
     }
 
     //5.定义点击下拉搜索结果函数
-    const handleSelect = (item: string) => {
-        setInputValue(item)
+    const handleSelect = (item: DataSourceType) => {
+        setInputValue(item.value)
         setSuggestions([])
         onSelect && onSelect(item)
     }
 
     //6.自定义下拉菜单样式
-    const renderTemplate = (item: string) => {
-        return renderOptions ? renderOptions(item) : item
+    const renderTemplate = (item: DataSourceType) => {
+        return renderOptions ? renderOptions(item) : item.value
     }
 
     return (<div className='viking-auto-complete'>
